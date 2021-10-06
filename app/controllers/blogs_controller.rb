086fdb118db49bda990d2bd1f37eed5f87ba00a4
lmq95 <4321namap@gmail.com>
2021-10-06 08:00:21 +0000

    [finish]4-6

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -1,7 +1,7 @@
 class BlogsController < ApplicationController
   def index
     @blogs = Blog.all
-    
+
   end
 
   def show
@@ -11,19 +11,26 @@ class BlogsController < ApplicationController
   def new
     @blog = Blog.new
   end
-  
+
   def create
     blog = Blog.new(blog_params)
     blog.save
-    redirect_to blogs_path(blog.i)
+    redirect_to blogs_path(blog.id)
   end
-  
+
   def edit
+    @blog = Blog.find(params[:id])
   end
   
+  def update
+    blog = Blog.find(params[:id])
+    blog.update(blog_params)
+    redirect_to blog_path(blog)
+  end
+
   private
   def blog_params
     params.require(:blog).permit(:title, :category, :body)
   end
-  
+
 end
